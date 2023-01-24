ff7f8c981c8842dfd6950b90f82ac4c7d8e931a3
Wallace Ip <ipch@sheridancollege.ca>
2023-01-24 14:04:52 -0500

    added setter

--- src/student/Student.java
+++ src/student/Student.java
@@ -12,15 +12,22 @@ public class Student {
     
     private String studentID;
     private String studentName;
-
     public Student(String studentID, String studentName) {
         this.studentID = studentID;
         this.studentName = studentName;
     }
 
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    
+    
     public String getStudentID() {
         return studentID;
     }
+    
+    
 
     public void setStudentID(String studentID) {
         this.studentID = studentID;
